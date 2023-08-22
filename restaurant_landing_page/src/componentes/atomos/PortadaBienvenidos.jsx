7afccec6aa33a73ccc5b7115231e9849a7d3f960
221236CarlosCruz <carlos.programacion@gruporyc.com.mx>
2023-08-22 09:43:05 -0600

    first commit

---
+++ restaurant_landing_page/src/componentes/atomos/PortadaBienvenidos.jsx
@@ -0,0 +1,29 @@
+import IconoPoots from "../../assets/img/IconoPoots.png";
+import LogoPootsBlanco from "../../assets/img/pootsBlanco.png";
+import "../../assets/styles/PortadaBienvenidos.css";
+
+function PortadaBienvenidos() {
+  return (
+    /*     <div className="background-image-container">
+          <div className="overlay">
+            <img src={LogoPootsBlanco} />
+            <button className="btn-bienvenidos">
+              <img src={IconoPoots} />
+              explorar
+            </button>
+          </div>
+        </div> */
+
+    <div className="background-image-container">
+      <div className="overlay">
+        <img className="logo" src={LogoPootsBlanco} alt="Logo Poots" />
+        <button className="btn-bienvenidos">
+          <img src={IconoPoots} alt="Icono Poots" />
+          explorar
+        </button>
+      </div>
+    </div>
+
+  );
+}
+export default PortadaBienvenidos;
